ed707de90f266e872a132cfe3f20d52574830ff7
jon.kiparsky <jon.kiparsky@8560d056-ae00-11de-b0a2-13e92fe56a31>
2009-10-03 06:33:10 +0000

    Added javadoc

--- trunk/src/phylogenySurvey/Link.java
+++ trunk/src/phylogenySurvey/Link.java
@@ -2,24 +2,46 @@ package phylogenySurvey;
 
 import org.jdom.Element;
 
+/**
+ * Represents the connection between two objects, stored internally as
+ * "oneLabel" and "otherLabel". 
+ * Linked objects can be SelectableLinkableObjects, which means Nodes
+ * and OrganismLabels. The field names suggest the latter are meant. I
+ * have not yet determined which are currently Linked in practice.
+ */
 public class Link {
 	
 	private SelectableLinkableObject oneLabel;
 	private SelectableLinkableObject otherLabel;
 	
+/**
+ * Takes two SelectableLinkableObjects (=JLabels with benefits) 
+ */
 	public Link(SelectableLinkableObject oneLabel, SelectableLinkableObject otherLabel) {
 		this.oneLabel = oneLabel;
 		this.otherLabel = otherLabel;
 	}
 
+/**
+ * Returns one of the two objects linked. 
+ */
 	public SelectableLinkableObject getOneLabel() {
 		return oneLabel;
 	}
 
+/**
+ *Returns the other of the two objects linked. 
+ */
 	public SelectableLinkableObject getOtherLabel() {
 		return otherLabel;
 	}
-	
+
+/**
+ * Creates an XML object with the contents of both objects connected by
+ * this Link. "Contents" = output of object's save() method, meaning two
+ * Elements are created and concatenated into one, which is the returned
+ * by this method.
+ */	
 	public Element save() {
 		Element e = new Element("Link");
 		Element s1e = new Element("FirstSLO");
