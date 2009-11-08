cb8fabff2f2260f2764c2a2fc2a8d8739ad34e1e
jon.kiparsky <jon.kiparsky@8560d056-ae00-11de-b0a2-13e92fe56a31>
2009-11-08 23:24:38 +0000

    Failed to add Connection.java, here it is

---
+++ branches/rewrite/src/tbs/model/Connection.java
@@ -0,0 +1,56 @@
+//TBS version 0.4
+//represents a connection between two node objects
+//impleented to make connections selectable
+
+package tbs.model;
+
+public class Connection extends ModelElement
+{
+
+	private Node toNode;
+	private Node fromNode;
+
+
+	/**
+	* Connection registers a connection between two nodes
+	*/
+	public Connection(Node to, Node from)
+	{
+		toNode=to;
+		fromNode=from;
+	}
+	
+	/**
+	* Returns the target node of this connection
+	*/
+	public Node getToNode()
+	{
+		return toNode;
+	}
+		
+	/**
+	* Returns the origin node of this connection
+	*/
+	public Node getFromNode()
+	{
+		return fromNode;
+	}
+
+	/**
+	* We might use this (in future) to resolve connection/node collisions
+	*/
+	public boolean collidesWith(ModelElement e)
+	{
+		return false;
+	}
+
+	/**
+	* Returns true for a point on the line, as determined algebraically
+	*/
+	public boolean contains (int x, int y)
+	{
+
+		//Glenn: here's your stub
+		return false;
+	}
+}
