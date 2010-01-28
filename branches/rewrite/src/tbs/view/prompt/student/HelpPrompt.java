3be8ded21a11462e38f0687e6407fda8b7926fee
andrew.schonfeld@bbh.com <andrew.schonfeld@bbh.com@8560d056-ae00-11de-b0a2-13e92fe56a31>
2010-01-28 11:40:21 +0000

    Fixed styling on button information headers in the Help menu.

--- branches/rewrite/src/tbs/view/prompt/student/HelpPrompt.java
+++ branches/rewrite/src/tbs/view/prompt/student/HelpPrompt.java
@@ -22,17 +22,17 @@ import tbs.view.prompt.Prompt;
 import tbs.view.prompt.buttons.HelpPromptButtonType;
 
 /**
-* HelpPrompt is called when the Help button is pressed; it displays a
-* help window which can be switched between several categories of help.
-* The help strings are read from several .properties files, and can be
-* changed by modifying those files. <br>
-* "Introduction" is read from "instructions.properties", property name
-* "instrIntro"
-* "Instructions" tab is read from "instructions.properties", properties
-* "InstrDir", "InstrDir2", and "InstrDir3".
-* "Button Info" is read from "help.properties". 
-* "Survey Status" is generated based on the current state of the Model. 
-*/
+ * HelpPrompt is called when the Help button is pressed; it displays a
+ * help window which can be switched between several categories of help.
+ * The help strings are read from several .properties files, and can be
+ * changed by modifying those files. <br>
+ * "Introduction" is read from "instructions.properties", property name
+ * "instrIntro"
+ * "Instructions" tab is read from "instructions.properties", properties
+ * "InstrDir", "InstrDir2", and "InstrDir3".
+ * "Button Info" is read from "help.properties". 
+ * "Survey Status" is generated based on the current state of the Model. 
+ */
 public class HelpPrompt extends Prompt
 {
 
@@ -52,10 +52,10 @@ public class HelpPrompt extends Prompt
 	HelpPromptButtonType[] options = HelpPromptButtonType.values();
 	HelpPromptButtonType selectedOption;
 	int buttonHeight;
-	
+	int buttonHeaderWidth;
 	List<String> introduction;
 	List<String> instructions;
-	
+
 	List<String[]> buttonInfo;
 	int buttonsLines = -1;
 	List<String> buttonHeaders;
@@ -74,31 +74,32 @@ public class HelpPrompt extends Prompt
 			if(!bt.isAdmin() && !TBSButtonType.HELP.equals(bt))
 			{
 				buttonInfo.add(new String[]{bt.getText(), 
-					helpProps.getProperty("help_" + bt.getText())});
+						helpProps.getProperty("help_" + bt.getText())});
 			}
 		}
 		buttonInfo.add(new String[]{"Questions", helpProps.getProperty("help_123")});
 		buttonHeaders = new LinkedList<String>();
+		buttonHeaderWidth = 0;
 		buttonTexts = new LinkedList<List<String>>();
 	}
 
 	/**
-	* This prompt does not accept keyboard input
-	*/
+	 * This prompt does not accept keyboard input
+	 */
 	public void keyPressed(KeyEvent e){}
 
 	/**
-	* This prompt does not accept keyboard input
-	*/
+	 * This prompt does not accept keyboard input
+	 */
 	public void keyTyped(KeyEvent e){}
 
 	/**
-	* On a mouse click, this method checks whether the event occurred
-	* within a button. If it occurred within the close button, the {@link
-	* setFinished} method is used to tell the prompt to close itself. If
-	* the event is located within one of the selector buttons, that
-	* option is displayed.
-	*/
+	 * On a mouse click, this method checks whether the event occurred
+	 * within a button. If it occurred within the close button, the {@link
+	 * setFinished} method is used to tell the prompt to close itself. If
+	 * the event is located within one of the selector buttons, that
+	 * option is displayed.
+	 */
 	public void mousePressed(MouseEvent e) {
 		if(closeButton.contains(e.getPoint()))
 			setFinished(true);
@@ -111,8 +112,8 @@ public class HelpPrompt extends Prompt
 	}
 
 	/**
-	* Instructions for rendering this object. 
-	*/
+	 * Instructions for rendering this object. 
+	 */
 	public void paintComponent(Graphics2D g2) 
 	{
 		this.g2 = g2;
@@ -134,16 +135,16 @@ public class HelpPrompt extends Prompt
 						instrProps.getProperty("instrDir"),
 						promptSize.width - padding.width * 2);
 
-		      List<String> directions2 = TBSGraphics.breakStringByLineWidth(g2,
-            		instrProps.getProperty("instrDir2"),
-            		promptSize.width - padding.width * 2);
-      		List<String> directions3 = TBSGraphics.breakStringByLineWidth(g2,
-            		instrProps.getProperty("instrDir3"),
-        	   	 	promptSize.width - padding.width * 2);
-      		instructions.add("\n");
+				List<String> directions2 = TBSGraphics.breakStringByLineWidth(g2,
+						instrProps.getProperty("instrDir2"),
+						promptSize.width - padding.width * 2);
+				List<String> directions3 = TBSGraphics.breakStringByLineWidth(g2,
+						instrProps.getProperty("instrDir3"),
+						promptSize.width - padding.width * 2);
+				instructions.add("\n");
 				instructions.addAll(directions2);
-      		instructions.add("\n");
-      		instructions.addAll(directions3);
+				instructions.add("\n");
+				instructions.addAll(directions3);
 
 			}
 			text = instructions;
@@ -180,8 +181,9 @@ public class HelpPrompt extends Prompt
 				promptSize.width - padding.width * 2,
 				buttonHeight,TBSGraphics.selectedPromptTextColor);
 		helpStringY += buttonHeight;
-		
+
 		if(HelpPromptButtonType.BUTTON_INFO.equals(selectedOption)){
+			buttonHeaderWidth = TBSGraphics.getStringBounds(g2, "Questions").width + (TBSGraphics.buttonsXPadding * 2);
 			for(int i=0;i<buttonTexts.size();i++)
 				drawText(buttonTexts.get(i), buttonHeaders.get(i));
 		}else
@@ -189,28 +191,33 @@ public class HelpPrompt extends Prompt
 	}
 
 	/**
-	* Determines values of several interesting numbers, including the
-	* center point of the applet, an anchor point for the top left corner
-	* of the Prompt and locations of the close and selector buttons. 
-	*/
+	 * Determines values of several interesting numbers, including the
+	 * center point of the applet, an anchor point for the top left corner
+	 * of the Prompt and locations of the close and selector buttons. 
+	 */
 	public void calculateValues(int lineCount) {
 		buttonHeight = TBSGraphics.textHeight + padding.height;
-		promptSize.setSize(promptSize.width, (TBSGraphics.textHeight * lineCount) + 
-				(padding.height * (lineCount + 1)) + buttonHeight);
+		
+		int height = (TBSGraphics.textHeight * lineCount) + 
+			(padding.height * (lineCount + 1)) + buttonHeight;
+		if(HelpPromptButtonType.BUTTON_INFO.equals(selectedOption))
+			height += (buttonInfo.size() * TBSGraphics.paddingWidth);
+		
+		promptSize.setSize(promptSize.width, height);
 		int centerX = model.getApplet().getWidth() / 2;
 		int centerY = model.getApplet().getHeight() / 2;
 		anchorPoint = new Point(centerX - (promptSize.width / 2), 
 				centerY - (promptSize.height / 2));
 		closeButton = new Rectangle((anchorPoint.x + promptSize.width) -
-			buttonHeight, anchorPoint.y,buttonHeight, buttonHeight);
+				buttonHeight, anchorPoint.y,buttonHeight, buttonHeight);
 		helpOptions = new Rectangle(anchorPoint.x, 
-			anchorPoint.y + (promptSize.height - buttonHeight),
-			promptSize.width, buttonHeight);
+				anchorPoint.y + (promptSize.height - buttonHeight),
+				promptSize.width, buttonHeight);
 	}
 
 	/**
-	* Creates the frame for this Prompt
-	*/
+	 * Creates the frame for this Prompt
+	 */
 	public void drawBox() {
 		Rectangle box = new Rectangle(anchorPoint.x-2, anchorPoint.y-2, promptSize.width+4, promptSize.height+4);
 		g2.setColor(Color.lightGray);
@@ -222,8 +229,8 @@ public class HelpPrompt extends Prompt
 	}
 
 	/**
-	* Draws an array of Strings to the screen
-	*/
+	 * Draws an array of Strings to the screen
+	 */
 	public void drawText(List<String> lines) {
 		int startX = anchorPoint.x + padding.width;
 		for(String line : lines){
@@ -233,31 +240,32 @@ public class HelpPrompt extends Prompt
 	}
 
 	/**
-	* Draws an array of Strings to the screen, plus a header
-	*/	
+	 * Draws an array of Strings to the screen, plus a header
+	 */	
 	public void drawText(List<String> lines, String header) {
 		int startX = anchorPoint.x + padding.width;
 		TBSGraphics.drawCenteredString(g2, header, startX, helpStringY, 0, TBSGraphics.textHeight, TBSGraphics.selectedPromptTextColor);
-		startX += TBSGraphics.buttonsWidth;
+		startX += buttonHeaderWidth;
 		for(String line : lines){
 			drawString(line, startX, helpStringY);
 			helpStringY += TBSGraphics.textHeight + padding.height;
 		}
+		helpStringY += TBSGraphics.paddingWidth;
 	}
 
 	/**
-	*	Passes s, x, and y to drawString(String s, int x, int y, boolean
-	*	isSelected) with "false" as the final value.
-	*/
+	 *	Passes s, x, and y to drawString(String s, int x, int y, boolean
+	 *	isSelected) with "false" as the final value.
+	 */
 	public void drawString(String s, int x, int y){
 		drawString(s, x, y, false);
 	}
 
 	/**
-	* Calls the drawCenteredString method from {@link TBSGraphics} to put
-	* a string on the screen. Default color for selected text is the same
-	* as used for EmptyNode, but this can be changed. 
-	*/
+	 * Calls the drawCenteredString method from {@link TBSGraphics} to put
+	 * a string on the screen. Default color for selected text is the same
+	 * as used for EmptyNode, but this can be changed. 
+	 */
 	public void drawString(String s, int x, int y, boolean isSelected) {
 		if(s != null && s.length() > 0)
 			TBSGraphics.drawCenteredString(g2, s, x, y, 0, TBSGraphics.textHeight, 
@@ -265,10 +273,10 @@ public class HelpPrompt extends Prompt
 	}
 
 	/**
-	* Draws the close button and selector buttons. Buttons, as elsewhere
-	* in TBS, are not objects, but are simply painted on the screen and
-	* checked by contains() methods. 
-	*/
+	 * Draws the close button and selector buttons. Buttons, as elsewhere
+	 * in TBS, are not objects, but are simply painted on the screen and
+	 * checked by contains() methods. 
+	 */
 	public void drawButtons()
 	{
 		Rectangle buttonRect = new Rectangle(helpOptions.x, helpOptions.y,
@@ -288,9 +296,9 @@ public class HelpPrompt extends Prompt
 
 
 	/**
-	* Returns true if {@link MouseEvent} e occurs within the boundaries
-	* of any of this Prompt's buttons. 
-	*/
+	 * Returns true if {@link MouseEvent} e occurs within the boundaries
+	 * of any of this Prompt's buttons. 
+	 */
 	public boolean isOverButton(MouseEvent e){
 		if(closeButton.contains(e.getPoint()))
 			return true;
