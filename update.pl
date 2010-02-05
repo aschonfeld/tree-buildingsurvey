2076f344bdc657cbc0e61873feb628fadb8bef23
jon.kiparsky <jon.kiparsky@8560d056-ae00-11de-b0a2-13e92fe56a31>
2010-02-05 06:52:31 +0000

    Perl script to put current revision number into TBSTestSurvey.pl.

---
+++ update.pl
@@ -0,0 +1,20 @@
+#! /usr/bin/perl
+
+
+$str = `svn update lib`;
+ split(/ /,$str);
+chomp($revno = $_[2]);
+$revno =~ s/\.//;
+$revno ++;
+
+open(SCRIPT, "TBS.pl") ||
+	die "Coudn't open TBSTestSurvey.pl";
+
+open (OUTFILE, ">TBSTestSurvey.pl");
+while (<SCRIPT>)
+{
+	s/REVNO/$revno/;
+	print OUTFILE $_;
+}
+
+
