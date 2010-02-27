204a2bf035902989ae5d7830e1141d565c4cc47e
andrew.schonfeld@bbh.com <andrew.schonfeld@bbh.com@8560d056-ae00-11de-b0a2-13e92fe56a31>
2010-02-27 05:10:15 +0000

    Added database retrieval of student section information.

--- branches/Glenn/AdminApplication/src/admin/dao/AdminJdbcDao.java
+++ branches/Glenn/AdminApplication/src/admin/dao/AdminJdbcDao.java
@@ -6,7 +6,35 @@ import java.util.List;
 
 public class AdminJdbcDao extends BaseJdbcDao {
 	
-	private static final String SQL_LOAD_STUDENTS = "SELECT name, date, tree, Q1, Q2 FROM student_testdata ORDER BY name ASC";
+	private static final String SQL_LOAD_TREES = "SELECT name, date, tree, Q1, Q2 FROM student_testdata  ORDER BY name ASC";
+	private static final String SQL_LOAD_STUDENTS = "SELECT name, section FROM students ORDER BY name ASC";
+	
+	/**
+	 * This method is utilized by AdminApplication to retrieve the
+	 * student data (name, tree, Q1, Q2) from the table, trees.student_testdata.
+	 *       
+	 * @return the desired <code>List</code> of <code>String[]</code> objects,
+	 * 		   or <code>null</code> if there is no data.
+	 * 
+	 * @throws Exception
+	 *             if an SQLException occurs.
+	 * 
+	 */
+	public List<String[]> loadTrees() throws Exception{
+		System.out.println(String.format("Fetching Students. SQL is [%s].", SQL_LOAD_TREES));
+		ResultSetRowHandler<String[]> handler = new ResultSetRowHandler<String[]>() {
+			public String[] handle(ResultSet rs) throws SQLException {
+				String name, date, tree, Q1, Q2;
+				name = rs.getString(1);
+				date = rs.getDate(2).toString();
+				tree = rs.getString(3);
+				Q1 = rs.getBlob(4).toString();
+				Q2 = rs.getBlob(5).toString();
+				return new String[]{name,date,tree,Q1,Q2};
+			}
+		};
+		return executeQuery("trees", SQL_LOAD_TREES, handler, new InParam[]{});
+	}
 	
 	/**
 	 * This method is utilized by AdminApplication to retrieve the
@@ -23,14 +51,12 @@ public class AdminJdbcDao extends BaseJdbcDao {
 		System.out.println(String.format("Fetching Students. SQL is [%s].", SQL_LOAD_STUDENTS));
 		ResultSetRowHandler<String[]> handler = new ResultSetRowHandler<String[]>() {
 			public String[] handle(ResultSet rs) throws SQLException {
-				String name, tree, Q1, Q2;
+				String name, section;
 				name = rs.getString(1);
-				tree = rs.getString(3);
-				Q1 = rs.getBlob(4).toString();
-				Q2 = rs.getBlob(5).toString();
-				return new String[]{name,"",tree,Q1,Q2};
+				section = rs.getString(2);
+				return new String[]{name,section};
 			}
 		};
-		return executeQuery(SQL_LOAD_STUDENTS, handler, new InParam[]{});
+		return executeQuery("grades", SQL_LOAD_STUDENTS, handler, new InParam[]{});
 	}
 }
