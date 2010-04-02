860519853aa0d5a34987b4dd2e6ecad743398229
andrew.schonfeld@bbh.com <andrew.schonfeld@bbh.com@8560d056-ae00-11de-b0a2-13e92fe56a31>
2010-04-02 18:32:13 +0000

    Simplified the amount of code within TBS_Deployment.pl by moving utility functions to a separate file "common_functions.pl".  If you are going to use this strategy of cleaning up code in the future remember to put the line "1;" at the bottom of your utility script, apparently Perl needs the last expression in the file to return a true value.

--- trunk/Deployable/TBS_Deployable.pl
+++ trunk/Deployable/TBS_Deployable.pl
@@ -2,6 +2,7 @@
 
 use CGI;
 require "ctime.pl";
+require "common_functions.pl";
 
 
 # set some constants
@@ -537,38 +538,3 @@ sub load_graphing_survey {
   	print "</form>\n";
     print "</body></html>\n";
 }
-
-sub decrypt_pw {
-	my $cryptpw = $_[0];
-	my $pw = $_[1];
-	my $isok = 0;
-	
-	if(crypt($pw, time.$$) eq $cryptpw && $pw ne "" && $cryptpw ne ""){
-		$isok = 1;
-	}
-	$isok;
-}
-
-# Perl trim function to remove whitespace from the start and end of the string
-sub trim($)
-{
-	my $string = shift;
-	$string =~ s/^\s+//;
-	$string =~ s/\s+$//;
-	return $string;
-}
-# Left trim function to remove leading whitespace
-sub ltrim($)
-{
-	my $string = shift;
-	$string =~ s/^\s+//;
-	return $string;
-}
-# Right trim function to remove trailing whitespace
-sub rtrim($)
-{
-	my $string = shift;
-	$string =~ s/\s+$//;
-	return $string;
-}
-
---
+++ trunk/Deployable/common_functions.pl
@@ -0,0 +1,35 @@
+# Perl decryption function returning whether a password is valid or not
+sub decrypt_pw {
+	my $cryptpw = $_[0];
+	my $pw = $_[1];
+	my $isok = 0;
+	
+	if(crypt($pw, time.$$) eq $cryptpw && $pw ne "" && $cryptpw ne ""){
+		$isok = 1;
+	}
+	$isok;
+}
+
+# Perl trim function to remove whitespace from the start and end of the string
+sub trim($)
+{
+	my $string = shift;
+	$string =~ s/^\s+//;
+	$string =~ s/\s+$//;
+	return $string;
+}
+# Left trim function to remove leading whitespace
+sub ltrim($)
+{
+	my $string = shift;
+	$string =~ s/^\s+//;
+	return $string;
+}
+# Right trim function to remove trailing whitespace
+sub rtrim($)
+{
+	my $string = shift;
+	$string =~ s/\s+$//;
+	return $string;
+}
+1;
