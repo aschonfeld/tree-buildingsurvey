1b53dbcd4d1b881735fbbb713c2229b8cc66cadc
andrew.schonfeld@bbh.com <andrew.schonfeld@bbh.com@8560d056-ae00-11de-b0a2-13e92fe56a31>
2010-04-20 02:04:45 +0000

    Updated TBS Non-CGI Servlet

--- ServletApplet/ServletProjects/TBS_Servlet/src/com/tbs/servlet/DBServlet.java
+++ ServletApplet/ServletProjects/TBS_Servlet/src/com/tbs/servlet/DBServlet.java
@@ -14,6 +14,7 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.tbs.Utils;
 import com.tbs.dao.TBSJdbcDao;
 
 /**
@@ -47,6 +48,14 @@ public class DBServlet extends HttpServlet{
         List<String[]> returnData = new LinkedList<String[]>();
         if (userOption == null) {
             // simply display the students
+        	System.out.println("Displaying the students");
+        	try{
+        		returnData = dao.loadStudents();
+            }catch(Exception e){
+            	System.out.println("Error retrieving information from database");
+            }
+        } else if (userOption.equals("Login")) {
+        	// simply display the students
         	System.out.println("Displaying the students");
         	try{
         		returnData = dao.loadStudents();
@@ -113,51 +122,93 @@ public class DBServlet extends HttpServlet{
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected synchronized void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		String userOption = null;
+		userOption = request.getParameterValues("UserOption")[0];
 		ObjectInputStream inputFromApplet = null;
-        String[] student = null;        
-        PrintWriter out = null;
-        
-        try
-        {  
-            // get an input stream from the applet
-	        inputFromApplet = new ObjectInputStream(request.getInputStream());
-	        System.out.println("Connected");
-	    
-	        // read the serialized student data from applet        
-	        System.out.println("Reading data...");
-	        student = (String[]) inputFromApplet.readObject();
-	        System.out.println("Finished reading.");
-	        
-	        inputFromApplet.close();
-	        
-	        // use our business object to register the student
-	        // in the database
-	        System.out.println(student);
-	        try{
-	        	List<String[]> data = dao.loadStudentSurvey(student[0]);
-	        	int returnVal;
-	        	if(data.size() == 0)
-	        		returnVal = dao.insertStudentSurvey(student[0], student[1], student[2], student[3], "");
-	        	else
-	        		returnVal = dao.updateStudentSurvey(student[0], student[1], student[2], student[3], "");
-	        	if(returnVal == 1)
-	        		System.out.println("Survey data has been updated");
-	        }catch(Exception e){
-	        	System.out.println("Error retrieving information from database");
-	        }
-	        
-	        System.out.println("Complete.");
-	         
-	        // send back a confirmation message to the applet
-            out = new PrintWriter(response.getOutputStream());
-            response.setContentType("text/plain");
-            out.println("confirmed");
-            out.flush();
-            out.close();          
-            
-        }catch (Exception e){
+		String[] student = null;        
+		PrintWriter out = null;
+
+		try
+		{  
+			// get an input stream from the applet
+			inputFromApplet = new ObjectInputStream(request.getInputStream());
+			System.out.println("Connected");
+
+			// read the serialized student data from applet        
+			System.out.println("Reading data...");
+			student = (String[]) inputFromApplet.readObject();
+			System.out.println("Finished reading.");
+
+			inputFromApplet.close();
+
+			if (userOption.equals("Login")) {
+				String[] studentData = new String[]{"","","","","","",""};
+				String savedPass = "", section="";
+		        try{
+		        	List<String[]> results = dao.loadStudentLogin(student[0]);
+		        	if(results.size() > 0){
+		        		section = results.get(0)[0];
+		        		savedPass = results.get(0)[1];
+		        		if(!Utils.validate(student[0], student[1], savedPass)){
+				        	List<String[]> data = dao.loadStudentSurvey(student[0]);
+				            	if(data.size() > 0){
+				            		studentData[0] = student[0];
+				            		studentData[1] = data.get(0)[0];
+				            		studentData[2] = data.get(0)[1];
+			            			studentData[3] = data.get(0)[2];
+			            			studentData[4] = data.get(0)[3];
+			            			studentData[5] = "";
+			            			studentData[6] = section;
+				            	}else{
+				            		studentData[0] = student[0];
+				            		studentData[6] = section;
+				            	}
+				        	}
+		        	}else
+		        		studentData = new String[]{"","","","","","",""};
+		        }catch(Exception e){
+		        	System.out.println("Error retrieving information from database");
+		        }
+		        try
+		        {
+		        	ObjectOutputStream outputToApplet = new ObjectOutputStream(response.getOutputStream());
+		        	System.out.println("Sending data to applet...");
+		        	outputToApplet.writeObject(studentData);
+		        	outputToApplet.flush();
+		        	outputToApplet.close();
+		        	System.out.println("Data transmission complete.");
+		        }catch (IOException e){
+		        	e.printStackTrace(); 
+		        }
+			} else if (userOption.equals("Save")) {
+
+				try{
+					List<String[]> data = dao.loadStudentSurvey(student[0]);
+					int returnVal;
+					if(data.size() == 0)
+						returnVal = dao.insertStudentSurvey(student[0], student[1], student[2], student[3], "");
+					else
+						returnVal = dao.updateStudentSurvey(student[0], student[1], student[2], student[3], "");
+					if(returnVal == 1)
+						System.out.println("Survey data has been updated");
+				}catch(Exception e){
+					System.out.println("Error retrieving information from database");
+				}
+
+				System.out.println("Complete.");
+
+				// send back a confirmation message to the applet
+				out = new PrintWriter(response.getOutputStream());
+				response.setContentType("text/plain");
+				out.println("confirmed");
+				out.flush();
+				out.close(); 
+			}
+
+
+		}catch (Exception e){
 			e.printStackTrace();    
-        }
+		}
 
 	}
 
